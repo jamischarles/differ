a123dd872f195bc76479438dc6d1693d4c778572
Jamis Charles <jacharles@paypal.com>
2019-04-13 23:35:17 -0600

    WIN: Url MVP works!!!

---
+++ diff.js
@@ -0,0 +1,251 @@
+// FIXME: use this as basis for unit tests..
+// This is the 1-2 hard things that Marco Arment talked about... Everything else is pretty easy-ish...
+var colors = require('colors');
+
+colors.setTheme({
+  remove: ['red', 'bgRed'],
+  add: ['green', 'bgGreen'],
+});
+
+// FIXME: GET features from here...
+// TODO: make use cases with LOOOONG urls much more friendly...
+// lots of options... like hiding common parts. Lining things up nicely.
+// Splitting up the url by parts and keeping it down down...
+// Hiding all the parts of the url (like host, etc, that are the same...)
+// for input, add a line split, and line items... Make it really easy to work with it and show where the split will e
+// Make it not explode if you remove parts of the url
+
+// TODO:
+// 1) Write a quick assertion for the previous use cases (add proper unit testing framework later).
+// Try tap? or something else from substack?
+
+// TODO: can we use the common chunks as a splitting point? So we remove those pieces from future matches?
+//
+// TODO: set up repo and track differences...
+
+function poorMansCharDiff(str1, str2) {
+  // Goal: find the common parts between 2 strings. (if % common is very low, then abandon char diffing...)
+  // let's start with high threshold, 3 char chunks...
+  // this is fairly similar to the island algo challenge I did at FB.
+
+  // take first 2 chars, and see if they match up with any part of string2.
+  // Q: Do I want to do this manually or use builtins? A: For now, let's use builtins...
+  // FIXME: add char length check... or a minimum length to abandon this check...
+  // TODO: make this recursive
+
+  // FIXME: make this better and clean this up...
+  var str1Matches = [];
+  var str2Matches = [];
+
+  // we want to keep track of both, because after a match, we don't want to revisit the same part of str1. We want to look forward...
+  // TODO: Does this mean we should keep track of match points and ignore those during the actual walking of the string?
+  // FIXME: This can and should be improved...
+  var str1Pos = 0;
+  var str2Pos = 0;
+
+  // run until we've reached the end of the string
+  while (str1Pos < str1.length) {
+    // console.log('str1Pos', str1Pos);
+    // FIXME: this can / should be improved with a better matching algorithm...
+    // FIXME: Using bigger number (2) yields greater accuracy... Change to 3?
+    var chunk = str1.slice(str1Pos, str1Pos + 3); // 2 chars at a time?
+    var matchIndex = str2.indexOf(chunk, str2Pos); // does this chunk exist in 2nd string? Starting at str2Pos (so we don't re-match the same parts over and over again)
+
+    if (matchIndex > -1) {
+      // if it matches, find end of match (start of diff)
+      // Q: Should we advance one char at a time, or keep using indexOf? Test perf later...
+      // find end of match...
+      var res = findMatchStartEnd(str1, str2, str1Pos, matchIndex);
+
+      // console.log('res', res);
+
+      str1Pos = res.str1.end + 1;
+      str2Pos = res.str2.end + 1;
+
+      // FIXME: this seems like a really awkward data structure for ranges of strings...
+      // maybe it'd be better to split the string into an array, and pass metadata along with it (match) or no match...
+      // then when we console we can assemble the string with coloring that way
+      str1Matches.push({start: res.str1.start, end: res.str1.end});
+      str2Matches.push({start: res.str2.start, end: res.str2.end});
+    } else {
+      // move on to next chunk
+      str1Pos += 2;
+    }
+  }
+
+  // assemble strings to output diff report...
+  //
+  return [
+    splitStringByMatch(str1, str1Matches),
+    splitStringByMatch(str2, str2Matches),
+  ];
+
+  // USED FOR Colorizing the output. FIXME: use this for command line testing, and/or diff CLI
+  // console.log('');
+  // console.log(colors.red('-'), colorizeDiffLine(str1, 'remove', str1Matches));
+  // console.log(colors.green('+'), colorizeDiffLine(str2, 'add', str2Matches));
+}
+
+// returns start, end of matching string positions for two strings
+// @params: first 2 strings. Then the positions where they match up
+// This will search forward and backwards in the strings to find match boundaries
+// FIXME: simplify?
+function findMatchStartEnd(str1, str2, str1Pos, str2Pos) {
+  // find start of match (verify if str1Pos:str2Pos is actual start of match, or some preceeding chars are good for match)
+  // go backwards searching for earlier match than passed in (happens sometimes...0
+  while (str1Pos > -1 && str2Pos > -1) {
+    // break on first deviation
+    if (str1.charAt(str1Pos) != str2.charAt(str2Pos)) {
+      // reset str1Pos and str2Pos to last known match (previous iteration)
+      str1Pos++;
+      str2Pos++;
+      break;
+    }
+
+    // if we've reached start of string, bail
+    if (str1Pos === 0 || str2Pos === 0) break;
+
+    str1Pos--;
+    str2Pos--;
+  }
+
+  // start at match positions for each string, and step forward one at a time...
+  for (var i = 0; i < str1.length; i++) {
+    // break on first deviation
+    if (str1.charAt(str1Pos + i) != str2.charAt(str2Pos + i)) {
+      break;
+    }
+  }
+
+  return {
+    str1: {
+      start: str1Pos,
+      end: str1Pos + i,
+    },
+    str2: {
+      start: str2Pos,
+      end: str2Pos + i,
+    },
+  };
+}
+
+// returns an array of objects with string and metadata
+// they are split by whether they are matches or not
+// [match, nomatch, match]
+//
+// for now just [{value:'', meta: {isMatch: true}}]
+// FIXME: can we change this so this array is created during the actual match process?
+// FIXME: Simplify this... With unit tests... When we get there...
+function splitStringByMatch(str, diffData) {
+  // split the strings according to where the matches are
+  let arr = [];
+
+  let buffer = '';
+  let currentBufferIsMatch; // boolean. When there's a change, we save buffer off to arr[]
+
+  for (let i = 0; i < str.length; i++) {
+    // when there is a change in match/no-match, save it to array, and clear buffer
+    let isMatch = isNumberInRange(i, diffData);
+
+    if (i === 0) {
+      currentBufferIsMatch = isMatch;
+    }
+
+    if (currentBufferIsMatch !== isMatch) {
+      // there's a change in matchStatus, so flush the buffer
+      arr.push({
+        value: buffer,
+        meta: {
+          isMatch: currentBufferIsMatch,
+        },
+      });
+
+      buffer = '';
+      currentBufferIsMatch = isMatch;
+    }
+
+    // save the letter to buffer
+    buffer += str[i];
+  }
+
+  return arr;
+}
+
+// FIXME: simplify this? Separate styling from what it is...
+// FIXME: move the colorizing to the test... And/or the command line util for diffing...?
+// Consider open sourcing the command line part for free...
+function colorizeDiffLine(str, addOrRemove, diffData) {
+  var output = str
+    .split('')
+    .map((letter, i) => {
+      // return letter;
+      // is this in range?
+      // if not, then it's not a string match, so highlight it (that means it's added, or removed)
+
+      // FIXME: clean up this nested if statement
+      if (isNumberInRange(i, diffData)) {
+        if (addOrRemove === 'remove') {
+          return colors.red(letter);
+        } else {
+          return colors.green(letter);
+        }
+      } else {
+        if (addOrRemove === 'remove') {
+          return colors.remove(letter);
+        } else {
+          return colors.add(letter);
+        }
+      }
+    })
+    .join(''); // turn back into string
+
+  return output;
+}
+
+// tells us if the number is in range of string matches
+function isNumberInRange(number, arrayOfRangeObj) {
+  // loop through the array of match obj. [{start:0, end:5}] etc
+  //
+  for (var i = 0; i < arrayOfRangeObj.length; i++) {
+    var start = arrayOfRangeObj[i].start;
+    var end = arrayOfRangeObj[i].end;
+
+    // is the number between start and end?
+    // 5-9. 5 and 9 are inclusive. Q: should end be <=?
+    if (number >= start && number < end) return true;
+  }
+
+  return false;
+}
+
+// TESTING SECTION...
+
+// FIXME: Abstract this stuff into tests...
+// being called from command line
+if (require.main === module) {
+  // TODO: add all these as unit tests...
+  poorMansCharDiff('3', '2');
+  poorMansCharDiff('3', '23');
+  poorMansCharDiff('32', '332');
+  poorMansCharDiff('jamis', 'jamis2');
+  poorMansCharDiff('jamis', 'jam3is2');
+  poorMansCharDiff('jamis', 'jamis22');
+  poorMansCharDiff('arstrsatjamis', 'jamis22');
+  poorMansCharDiff('arstrsatjamis', 'jamisarstrsatadam');
+
+  var firstUrl =
+    'https://www.msmaster.qa.paypal.com/signin?returnUri=https%3A%2F%2Fwww.msmaster.qa.paypal.com%2Fmyaccount%2Ftransfer&state=%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds%26currencyCode%3DUSD%26amount%3D123%26locale.x%3Den_US%26country.x%3DUS%26flowType%3Dsend';
+
+  var secondUrl = `https://www.msmaster.qa.paypal.com/signin?returnUri=https%3A%2F%2Fmsmaster.qa.paypal.com%2Fmyaccount%2Ftransfer%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds&currencyCode=USD&amount=120&locale.x=en-us&country.x=US&flowType=send&onboardData={"country.x"%3A"US"%2C"locale.x"%3A"en-us"%2C"intent"%3A"paypalme"%2C"redirect_url"%3A"https%3A%2F%2Fmsmaster.qa.paypal.com%2Fmyaccount%2Ftransfer%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds%26currencyCode%3DUSD%26amount%3D120%26locale.x%3Den-us%26country.x%3DUS%26flowType%3Dsend"%2C"sendMoneyText"%3A"Rosalind Douglas %24120.00"}`;
+
+  poorMansCharDiff(firstUrl, secondUrl);
+  // else being required in...
+} else {
+  module.exports = {
+    diffChars: poorMansCharDiff,
+  };
+}
+
+// TODO: since we know that we are dealing with a url, can/should we use word boundaries like `&`, `.`, `:`, `/`, `=` to create delimeters that we can use to make diffing easier?
+//
+//
--- renderer.js
+++ renderer.js
@@ -1,3 +1,168 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const url = require('url');
+
+const diffUtils = require('./diff');
+
+// We can use pretty simple algorithms here...
+function urlDiff(urlStr1, urlStr2) {
+  // split into the various parts and diff each one...
+  const url1 = url.parse(urlStr1);
+  const url2 = url.parse(urlStr2);
+
+  console.log('url1', url1);
+  console.log('url2', url2);
+
+  // don't need to compare keys here, because all the keys are the same...
+  // FIXME: Can we use the json comparision module here? For now just hack it, and then maybe later we can replace the logic here...
+
+  var report = Object.keys(url1)
+    .map(key => {
+      // console.log('key', key);
+      // console.log('url1[key]', url1[key]);
+      // console.log('url2[key]', url2[key]);
+      //
+      // ignore these keys (redundant)
+      if (['search', 'path', 'href'].includes(key)) {
+        return null;
+      }
+
+      let diffObj = {
+        key,
+        values: [url1[key]], // if same, will only have one result
+        // diff: diffString(this.values),
+      };
+
+      // if they aren't the same...
+      if (url1[key] != url2[key]) {
+        diffObj.values.push(url2[key]);
+        // diffObj.diff = diffString(url1[key], url2[key]);
+        diffObj.diff = diffUtils.diffChars(url1[key], url2[key]);
+      }
+      return diffObj;
+    })
+    .filter(item => item !== null); // remove null items (no sparse array)
+
+  console.log('report', report);
+  return report;
+  // compare the parts
+  //
+  // store report on what parts are different (and where they are different (like an AST))
+  // array of differences?
+}
+
+// everything will be based on this...
+// How do I diff the 2 strings and generate a good AST-like report...?
+// let's use this for a minified, string diff... (make this work for a giant single word diff too? because wdiff only works for word spaces...)
+function diffString(str1, str2) {
+  let diffs = [];
+  diffs.push({
+    start: 5,
+    end: 10,
+    old: 'jamis',
+    new: 'jamis22',
+  });
+
+  return diffs;
+}
+
+// adds span with class names for styling the parts the changed.
+function renderDiffText(diffStrArr) {
+  let output = diffStrArr
+    .map(str => {
+      if (str.meta.isMatch) {
+        return str.value;
+      } else {
+        return `<span class="no-match">${str.value}</span>`;
+      }
+    })
+    .join(''); // turn into string from array
+
+  return output;
+}
+
+// output diff that is up/down...
+function renderTopDiff(str1, str2) {
+  var diffs = diffUtils.diffChars(str1, str2);
+  console.log('diff', diffs);
+
+  let output = `
+    <h2>Top/down diff</h2>
+    <div class="old no-wrap">- ${renderDiffText(diffs[0])}</div>
+    <div class="new no-wrap">+ ${renderDiffText(diffs[1])}</div>
+  `;
+
+  document.getElementById('topDiff').innerHTML = output;
+}
+
+// output the diff...
+// FIXME: consider using react for this?
+// Or vue. Or lit-html? Something that I don't need a compile step for...
+// FIXME: simplify this code A LOT...
+function renderSideDiff(data) {
+  var rows = data.map(diffItem => {
+    // no diff
+    if (diffItem.values.length === 1) {
+      return `<tr><td>${diffItem.key}</td><td>${diffItem.values[0]}</td><td>${
+        diffItem.values[0]
+      }</td></tr>`;
+    } else {
+      return `<tr><td>${diffItem.key}</td><td class="old">${renderDiffText(
+        diffItem.diff[0],
+      )}</td><td class="new">${renderDiffText(diffItem.diff[1])}</td></tr>`;
+    }
+  });
+
+  const output = `
+  <h2>Side by side diff</h2>
+<table border="1">
+  <tr>
+    <td>Part</td>
+    <td>Left side</td>
+    <td>Right side</td>
+  </tr>
+  ${rows}
+</table>
+  `;
+
+  document.getElementById('sideDiff').innerHTML = output;
+}
+//
+//
+// Each problem / use case can really be split into 2 parts: 1) The technical code (backend) to make the comparison correctly and efficiently (fast) and 2) displaying the data in a way that makes for a really really great UX.
+//
+//
+// TODO: figure out how to tab the sections...?
+// var firstUrl =
+//   'https://www.msmaster.qa.paypal.com/signin?returnUri=https%3A%2F%2Fwww.msmaster.qa.paypal.com%2Fmyaccount%2Ftransfer&state=%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds%26currencyCode%3DUSD%26amount%3D123%26locale.x%3Den_US%26country.x%3DUS%26flowType%3Dsend';
+//
+// var secondUrl = `https://www.msmaster.qa.paypal.com/signin?returnUri=https%3A%2F%2Fmsmaster.qa.paypal.com%2Fmyaccount%2Ftransfer%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds&currencyCode=USD&amount=120&locale.x=en-us&country.x=US&flowType=send&onboardData={"country.x"%3A"US"%2C"locale.x"%3A"en-us"%2C"intent"%3A"paypalme"%2C"redirect_url"%3A"https%3A%2F%2Fmsmaster.qa.paypal.com%2Fmyaccount%2Ftransfer%2Fsend%2Fexternal%2Fppme%3Fprofile%3Dsds%26currencyCode%3DUSD%26amount%3D120%26locale.x%3Den-us%26country.x%3DUS%26flowType%3Dsend"%2C"sendMoneyText"%3A"Rosalind Douglas %24120.00"}`;
+
+// for now just some hacky code...
+function runDiff(e) {
+  var el = document.getElementById('diff-block');
+  // console.log('el', el);
+  // console.log('el.innerHTML', el.innerHTML);
+  var arr = el.value.split('\n');
+
+  var str1 = arr[0];
+  var str2 = arr[1];
+
+  // FIXME: move this into the sideBySide function?
+  var data = urlDiff(str1, str2);
+
+  renderSideDiff(data);
+  renderTopDiff(str1, str2);
+}
+
+function addEventlisteners() {
+  var el = document.getElementById('button');
+  el.addEventListener('click', runDiff);
+}
+
+function init() {
+  addEventlisteners();
+}
+
+init();
